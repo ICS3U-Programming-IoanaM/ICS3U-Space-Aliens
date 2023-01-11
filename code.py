c255407bb90c741cb9eaaabb01a05c5309db378e
Ioana Marinescu <sh ./setup.shioana.marinescu@stu.ocsb.ca>
2023-01-11 19:23:24 +0000

    fixed the bug. now show up on all the screen

--- code.py
+++ code.py
@@ -13,7 +13,7 @@ import ugame
 def game_scene():
     # imports background image and allows it to be displayed
     image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
-    background = stage.Grid(image_bank_background)
+    background = stage.Grid(image_bank_background, 10, 8)
 
     # displays the background ands it to a layer
     game = stage.Stage(ugame.display, 60)
