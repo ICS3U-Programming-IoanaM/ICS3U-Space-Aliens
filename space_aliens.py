fed4b6365a1a09bf789e7003edef3d4ae9813f24
Ioana <99274396+Ioana-Mar@users.noreply.github.com>
2023-01-10 21:14:46 -0500

    Delete space_aliens.py

--- space_aliens.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# Copyright (c) 2022 Ioana Marinescu All rights reserved.
-# Created By: Ioana Marinescu
-#
-# Date: Jan. 10, 2022
-
-# displays a message
-print("hello world")
