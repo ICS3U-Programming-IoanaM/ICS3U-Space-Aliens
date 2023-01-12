2e22402cda879885915b2d53e11f53a183ed3e05
Ioana Marinescu <sh ./setup.shioana.marinescu@stu.ocsb.ca>
2023-01-12 19:12:06 +0000

    Completed CPT-03

--- code.py
+++ code.py
@@ -11,18 +11,28 @@ import ugame
 
 # main game_scene
 def game_scene():
-    # imports background image and allows it to be displayed
+    # imports image
     image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
+    image_bank_sprites = stage.Bank.from_bmp16("space_aliens.bmp")
+
+    # sets the image(s) in a grid
     background = stage.Grid(image_bank_background, 10, 8)
+    ship = stage.Sprite(image_bank_sprites, 5, 75, 66)
 
     # displays the background ands it to a layer
     game = stage.Stage(ugame.display, 60)
-    game.layers = [background]
+    game.layers = [ship] + [background]
     game.render_block()
 
     # game loop
     while True:
-        pass
+        # get user input
+
+        # update game logic
+
+        # redraw the Sprites
+        game.render_sprites([ship])
+        game.tick()
 
 
 if __name__ == "__main__":
