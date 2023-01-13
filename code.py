b98da69064f1ac21da347333f00e4482af87e418
Ioana Marinescu <sh ./setup.shioana.marinescu@stu.ocsb.ca>
2023-01-13 18:47:14 +0000

    completed CPT-04

--- code.py
+++ code.py
@@ -19,14 +19,32 @@ def game_scene():
     background = stage.Grid(image_bank_background, 10, 8)
     ship = stage.Sprite(image_bank_sprites, 5, 75, 66)
 
-    # displays the background ands it to a layer
+    # displays the layers
     game = stage.Stage(ugame.display, 60)
     game.layers = [ship] + [background]
     game.render_block()
 
     # game loop
     while True:
-        # get user input
+    # get user input
+        keys = ugame.buttons.get_pressed()
+
+        if keys & ugame.K_X:
+            print("A")
+        if keys & ugame.K_O:
+            print("B")
+        if keys & ugame.K_START:
+            print("Start")
+        if keys & ugame.K_SELECT:
+            print("select")
+        if keys & ugame.K_RIGHT:
+            ship.move(ship.x + 1, ship.y)
+        if keys & ugame.K_LEFT:
+            ship.move(ship.x - 1, ship.y)
+        if keys & ugame.K_UP:
+            ship.move(ship.x, ship.y - 1)
+        if keys & ugame.K_DOWN:
+            ship.move(ship.x, ship.y + 1)
 
         # update game logic
 
