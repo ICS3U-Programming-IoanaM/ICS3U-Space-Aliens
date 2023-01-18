df9f95bd018757c3ab1fe7a9fc4c81a6f98f43b7
Ioana Marinescu <sh ./setup.shioana.marinescu@stu.ocsb.ca>
2023-01-18 19:26:57 +0000

    Completed CPT-08

--- code.py
+++ code.py
@@ -9,12 +9,76 @@
 import constants
 import stage
 import ugame
+import time
+import random
+
+
+# main splash_scene
+def splash_scene():
+    # get sound ready
+    coin_sound = open("Assets/coin.wav", "rb")
+    sound = ugame.audio
+    sound.stop()
+    sound.mute(False)
+    sound.play(coin_sound)
+
+    # imports all images needed
+    image_bank_mt_background = stage.Bank.from_bmp16("Assets/mt_game_studio.bmp")
+
+    # sets the image(s) in a grid
+    background = stage.Grid(
+        image_bank_mt_background, constants.SCREEN_GRID_X, constants.SCREEN_GRID_Y
+    )
+
+    # used this program to split the image into tile:
+    #   https://ezgif.com/sprite-cutter/ezgif-5-818cdbcc3f66.png
+    background.tile(2, 2, 0)  # blank white
+    background.tile(3, 2, 1)
+    background.tile(4, 2, 2)
+    background.tile(5, 2, 3)
+    background.tile(6, 2, 4)
+    background.tile(7, 2, 0)  # blank white
+
+    background.tile(2, 3, 0)  # blank white
+    background.tile(3, 3, 5)
+    background.tile(4, 3, 6)
+    background.tile(5, 3, 7)
+    background.tile(6, 3, 8)
+    background.tile(7, 3, 0)  # blank white
+
+    background.tile(2, 4, 0)  # blank white
+    background.tile(3, 4, 9)
+    background.tile(4, 4, 10)
+    background.tile(5, 4, 11)
+    background.tile(6, 4, 12)
+    background.tile(7, 4, 0)  # blank white
+
+    background.tile(2, 5, 0)  # blank white
+    background.tile(3, 5, 0)
+    background.tile(4, 5, 13)
+    background.tile(5, 5, 14)
+    background.tile(6, 5, 0)
+    background.tile(7, 5, 0)  # blank white
+
+    # creates a stage for the background to be displayed
+    # and sets the frame rate to 60fps
+    game = stage.Stage(ugame.display, constants.FPS)
+    # sets layers, items show up in order
+    game.layers = [background]
+    # renders background + original location of the sprite
+    game.render_block()
+
+    # game loop
+    while True:
+        # waits for 2 seconds
+        time.sleep(2.0)
+        menu_scene()
 
 
 # main menu_scene
 def menu_scene():
     # imports all images needed
-    image_bank_background = stage.Bank.from_bmp16("Assets/space_aliens_background.bmp")
+    image_bank_background = stage.Bank.from_bmp16("Assets/mt_game_studio.bmp")
 
     # adds text objects
     text = []
@@ -80,6 +144,11 @@ def game_scene():
     background = stage.Grid(
         image_bank_background, constants.SCREEN_GRID_X, constants.SCREEN_GRID_Y
     )
+    # random background
+    for x_location in range(constants.SCREEN_GRID_X):
+        for y_location in range(constants.SCREEN_GRID_Y):
+            tile_picked = random.randint(1, 3)
+            background.tile(x_location, y_location, tile_picked)
 
     # sprites
     ship = stage.Sprite(
