51b17c2203c9cf59fc60e3fb9a1db6eaccbad21c
Ioana Marinescu <sh ./setup.shioana.marinescu@stu.ocsb.ca>
2023-01-19 16:32:52 +0000

    Completed CPT-09

--- code.py
+++ code.py
@@ -162,11 +162,19 @@ def game_scene():
         16,
     )
 
+    # creates multiple lasers for when we shoot
+    lasers = []
+    for laser_number in range(constants.TOTAL_NUMBER_OF_LASERS):
+        a_single_laser = stage.Sprite(
+            image_bank_sprites, 10, constants.OFF_SCREEN_X, constants.OFF_SCREEN_Y
+        )
+        lasers.append(a_single_laser)
+
     # creates a stage for the background to be displayed
     # and sets the frame rate to 60fps
     game = stage.Stage(ugame.display, constants.FPS)
     # sets layers, items show up in order
-    game.layers = [ship] + [alien] + [background]
+    game.layers = lasers + [ship] + [alien] + [background]
     # renders background + original location of the sprite
     game.render_block()
 
@@ -219,12 +227,28 @@ def game_scene():
             pass
 
         # updates game logic
-        # plays a sound if A button was button_just_pressed
+        # fores a laser if A button is pressed
         if a_button == constants.button_state["button_just_pressed"]:
-            sound.play(pew_sound)
+            for laser_number in range(len(lasers)):
+                if lasers[laser_number].x < 0:
+                    lasers[laser_number].move(ship.x, ship.y)
+                    sound.play(pew_sound)
+                    break
+
+        # each frame move the lasers that have been fired
+        for laser_number in range(len(lasers)):
+            if lasers[laser_number].x > 0:
+                lasers[laser_number].move(
+                    lasers[laser_number].x,
+                    lasers[laser_number].y - constants.LASER_SPEED,
+                )
+                if lasers[laser_number].y < constants.OFF_TOP_SCREEN:
+                    lasers[laser_number].move(
+                        constants.OFF_SCREEN_X, constants.OFF_SCREEN_Y
+                    )
 
         # redraws the Sprites
-        game.render_sprites([ship] + [alien])
+        game.render_sprites(lasers + [ship] + [alien])
         game.tick()
 
 
